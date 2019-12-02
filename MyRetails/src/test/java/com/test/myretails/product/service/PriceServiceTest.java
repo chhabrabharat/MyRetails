ed7a4e41acbdd08090770f5eaccd4032e8fbaeda
Bharat Chhabra <say2bharat.c@gmail.com>
2019-12-02 14:32:39 +0530

    Target Case Study

---
+++ MyRetails/src/test/java/com/test/myretails/product/service/PriceServiceTest.java
@@ -0,0 +1,16 @@
+package com.test.myretails.product.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PriceServiceTest {
+
+    @Test
+    void addPrice() {
+    }
+
+    @Test
+    void getPrices() {
+    }
+}
---
+++ MyRetails/src/test/java/com/test/myretails/product/service/ProductServiceTest.java
@@ -0,0 +1,24 @@
+package com.test.myretails.product.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void getCurrentPrice() {
+    }
+
+    @Test
+    void testGetCurrentPrice() {
+    }
+}
